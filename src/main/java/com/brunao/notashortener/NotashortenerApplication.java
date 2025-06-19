8afa85f2e3eefee840b97423b4f39f997948677b
brnrez <maciel2bruno@gmail.com>
2025-06-19 14:07:15 -0300

    Initial Commit

---
+++ src/main/java/com/brunao/notashortener/NotashortenerApplication.java
@@ -0,0 +1,13 @@
+package com.brunao.notashortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotashortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotashortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunao/notashortener/NotashortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunao.notashortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotashortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
