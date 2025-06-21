61991c5a8ad9e0d4f54ea1f959bb7c7e5cb49873
brnrez <maciel2bruno@gmail.com>
2025-06-21 03:35:01 -0300

    feat: added Swagger and removed Spring Docker dependencies

--- src/main/java/com/brunao/notashortener/NotashortenerApplication.java
+++ src/main/java/com/brunao/notashortener/NotashortenerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NotashortenerApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(NotashortenerApplication.class, args);
 	}
